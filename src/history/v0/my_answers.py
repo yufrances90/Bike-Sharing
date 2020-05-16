581086197fb1a8dec815d8395f505bec82d08639
yufrances90 <yufrances90@gmail.com>
2020-05-15 18:40:00 -0700

    feat: Implemented v1 activation function & forward pass

--- src/history/v0/my_answers.py
+++ src/history/v0/my_answers.py
@@ -20,7 +20,7 @@ class NeuralNetwork(object):
         #
         # Note: in Python, you can define a function with a lambda expression,
         # as shown below.
-        self.activation_function = lambda x : 1 / (1 + np.exp(-x))  # Replace 0 with your sigmoid calculation.
+        self.activation_function = lambda x : 0  # Replace 0 with your sigmoid calculation.
         
         ### If the lambda code above is not something you're familiar with,
         # You can uncomment out the following three lines and put your 
@@ -46,12 +46,11 @@ class NeuralNetwork(object):
         delta_weights_h_o = np.zeros(self.weights_hidden_to_output.shape)
         for X, y in zip(features, targets):
             
-            final_outputs, hidden_outputs = self.forward_pass_train(X)  
-            
+            final_outputs, hidden_outputs = self.forward_pass_train(X)  # Implement the forward pass function below
             # Implement the backproagation function below
-#             delta_weights_i_h, delta_weights_h_o = self.backpropagation(final_outputs, hidden_outputs, X, y, 
-#                                                                         delta_weights_i_h, delta_weights_h_o)
-#         self.update_weights(delta_weights_i_h, delta_weights_h_o, n_records)
+            delta_weights_i_h, delta_weights_h_o = self.backpropagation(final_outputs, hidden_outputs, X, y, 
+                                                                        delta_weights_i_h, delta_weights_h_o)
+        self.update_weights(delta_weights_i_h, delta_weights_h_o, n_records)
 
 
     def forward_pass_train(self, X):
@@ -62,12 +61,15 @@ class NeuralNetwork(object):
             X: features batch
 
         '''
-        
-        hidden_inputs = X.dot(self.weights_input_to_hidden)
-        hidden_outputs = self.activation_function(hidden_inputs)
+        #### Implement the forward pass here ####
+        ### Forward pass ###
+        # TODO: Hidden layer - Replace these values with your calculations.
+        hidden_inputs = None # signals into hidden layer
+        hidden_outputs = None # signals from hidden layer
 
-        final_inputs = hidden_outputs.dot(self.weights_hidden_to_output)
-        final_outputs = self.activation_function(final_inputs)
+        # TODO: Output layer - Replace these values with your calculations.
+        final_inputs = None # signals into final output layer
+        final_outputs = None # signals from final output layer
         
         return final_outputs, hidden_outputs
 
