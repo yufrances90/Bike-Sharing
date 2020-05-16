3dbab73a8e82e40b649abefeeae54ea50a230c0e
yufrances90 <yufrances90@gmail.com>
2020-05-15 22:54:28 -0700

    Finalized v1

--- src/my_answers.py
+++ src/my_answers.py
@@ -122,7 +122,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 100
-learning_rate = 0.1
-hidden_nodes = 2
+iterations = 2500
+learning_rate = 0.9
+hidden_nodes = 10
 output_nodes = 1
